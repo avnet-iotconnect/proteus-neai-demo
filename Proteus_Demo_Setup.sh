f221389538abc38d9b1779f0774cecf8f8f838d9
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2024-08-28 11:27:40 -0600

    Update Proteus_Demo_Setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- Proteus_Demo_Setup.sh
+++ Proteus_Demo_Setup.sh
@@ -5,10 +5,10 @@ apt-get upgrade -y
 apt-get install python3-pip -y
 pip3 install paho-mqtt==1.6.1
 apt-get install opus-tools -y
-cp -r /media/usbdrive/Proteus-NEAI-Demo-main /home/weston
-cd /home/weston/Proteus-NEAI-Demo-main/iotc-python-sdk-master-std-21-patch/iotconnect-sdk-1.0
+cp -r /media/usbdrive/proteus-neai-demo-main /home/weston
+cd /home/weston/proteus-neai-demo-main/iotc-python-sdk-master-std-21-patch/iotconnect-sdk-1.0
 python3 setup.py install
-cd /home/weston/Proteus-NEAI-Demo-main
+cd /home/weston/proteus-neai-demo-main
 pip3 install blue_st_sdk-1.5.0-py3-none-any.whl
-cp /home/weston/Proteus-NEAI-Demo-main/iotconnect.service /etc/systemd/system
+cp /home/weston/proteus-neai-demo-main/iotconnect.service /etc/systemd/system
 systemctl enable iotconnect
