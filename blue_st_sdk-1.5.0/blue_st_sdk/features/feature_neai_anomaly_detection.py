79a28d784c1594afd5b7c0ccf7fc337e9b267421
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2024-09-03 12:27:33 -0600

    Update feature_neai_anomaly_detection.py
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- blue_st_sdk-1.5.0/blue_st_sdk/features/feature_neai_anomaly_detection.py
+++ blue_st_sdk-1.5.0/blue_st_sdk/features/feature_neai_anomaly_detection.py
@@ -46,7 +46,6 @@ from blue_st_sdk.utils.number_conversion import NumberConversion
 from blue_st_sdk.utils.number_conversion import LittleEndian
 from blue_st_sdk.utils.blue_st_exceptions import BlueSTInvalidOperationException
 from blue_st_sdk.utils.blue_st_exceptions import BlueSTInvalidDataException
-sys.path.append("/home/weston/proteus_stuff/STM32MP157F_Demo")
 
 # CLASSES
 
@@ -161,7 +160,7 @@ class FeatureNEAIAnomalyDetection(Feature):
             dict["progress"] = int(str(self.get_progress(sample)))
             dict["status"] = str(self.get_status(sample))
             dict["similarity"] = int(str(self.get_similarity(sample)))
-            with open("/home/weston/Proteus-NEAI-Demo-main/upstream_data.json","w") as upstream_file:
+            with open("/home/weston/proteus-neai-demo-main/upstream_data.json","w") as upstream_file:
                 json.dump(dict, upstream_file)
         return result
 
